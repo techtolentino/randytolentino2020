d7282ab72ad04a1940bdc63dbd4a589896ee8d52
techtolentino <makertolentino@gmail.com>
2020-05-02 06:08:48 -0500

    copy update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,7 @@ const ContactPage = () => (
         <div className="rt-hero-contact-content">
           <hr className="divider-alt" />
           <h1 className="rt-hero-contact-title">contact</h1>
-          <p>I’m super easy to reach.<br /><br />If you’d like to connect on anything related to career opportunities, software development, design, or freelance writing, please feel free to get in touch. You can <a className="text-link-dark" href="mailto:makertolentino@gmail.com" target="_blank" rel="noopener noreferrer">send me a message</a> or find me on the socials.</p>
+          <p>I’m super easy to reach.<br /><br />If you’d like to connect on anything related to career growth, code, design, or writing, please feel free to get in touch. You can <a className="text-link-dark" href="mailto:makertolentino@gmail.com" target="_blank" rel="noopener noreferrer">send me a message</a> or find me on the socials.</p>
 
           <div className="rt-block-container">
             <EmailForm title="Stay up to date" />
