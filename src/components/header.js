7b9ee5a9fa6823f4194ccfa74e2d963bcbc24d19
techtolentino <makertolentino@gmail.com>
2020-05-16 14:41:14 -0500

    add mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "gatsby"
 
-export default () => {
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuActiveClass: '',
+    }
+  }
 
-  return (
-    <header className="rt-header">
-      <div className="rt-container">
-        <Link className="rt-header-brand" to="/">R.</Link>
-        <nav className="rt-header-nav">
+  triggerMenu = () => {
+    if (this.state.menuActiveClass === '') {
+      this.setState({ menuActiveClass: 'is-active' })
+    } else {
+      this.setState({ menuActiveClass: '' })
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <header className="rt-header" >
+          <div className="rt-container">
+            <Link className="rt-header-brand" to="/">Randy Tolentino</Link>
+            <button className={`hamburger hamburger--spin ${this.state.menuActiveClass}`} type="button" onClick={this.triggerMenu}>
+              <span className="hamburger-box">
+                <span className="hamburger-inner"></span>
+              </span>
+            </button>
+            <nav className="rt-header-nav">
+              <Link className="rt-header-nav-item" to="/about">about</Link>
+              <Link className="rt-header-nav-item" to="/work">work</Link>
+              <Link className="rt-header-nav-item" to="/writing">writing</Link>
+              <Link className="rt-header-nav-item" to="/contact">contact</Link>
+            </nav>
+          </div>
+        </header>
+        <aside className={`rt-header-nav-mobile ${this.state.menuActiveClass}`}>
           <Link className="rt-header-nav-item" to="/about">about</Link>
           <Link className="rt-header-nav-item" to="/work">work</Link>
           <Link className="rt-header-nav-item" to="/writing">writing</Link>
           <Link className="rt-header-nav-item" to="/contact">contact</Link>
-        </nav>
-      </div>
-    </header>
-  )
+        </aside>
+      </>
+    )
+  }
 }
+
+export default Header
