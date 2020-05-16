11fa09918ba365939728b101c39840be23012546
techtolentino <makertolentino@gmail.com>
2020-05-16 16:03:25 -0500

    static mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -2,34 +2,6 @@ import React, { Component } from 'react';
 import { Link } from "gatsby"
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuActiveClass: '',
-    }
-  }
-
-  componentDidMount() {
-    window.onscroll = () => this._handleScroll()
-  }
-
-  _handleScroll() {
-    if (document.documentElement.scrollTop > 200) {
-      this.setState({ menuActiveClass: '' });
-    }
-  }
-
-  triggerMenu = () => {
-    if (this.state.menuActiveClass === '') {
-      this.setState({ menuActiveClass: 'is-active' })
-    } else {
-      this.setState({ menuActiveClass: '' })
-    }
-  }
-
-  closeMenu = () => {
-    this.setState({ menuActiveClass: '' })
-  }
 
   render() {
     return (
@@ -37,11 +9,7 @@ class Header extends Component {
         <header className="rt-header" >
           <div className="rt-container">
             <Link className="rt-header-brand" to="/">Randy Tolentino</Link>
-            <button className={`hamburger hamburger--spin ${this.state.menuActiveClass}`} type="button" onClick={this.triggerMenu}>
-              <span className="hamburger-box">
-                <span className="hamburger-inner"></span>
-              </span>
-            </button>
+
             <nav className="rt-header-nav">
               <Link className="rt-header-nav-item" to="/about">about</Link>
               <Link className="rt-header-nav-item" to="/work">work</Link>
@@ -50,12 +18,6 @@ class Header extends Component {
             </nav>
           </div>
         </header>
-        <aside className={`rt-header-nav-mobile ${this.state.menuActiveClass}`}>
-          <a className="rt-header-nav-item" onClick={this.closemenu} href="/about">about</a>
-          <a className="rt-header-nav-item" onClick={this.closemenu} href="/work">work</a>
-          <a className="rt-header-nav-item" onClick={this.closemenu} href="/writing">writing</a>
-          <a className="rt-header-nav-item" onClick={this.closemenu} href="/contact">contact</a>
-        </aside>
       </div>
     )
   }
