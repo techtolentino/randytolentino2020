10db84644c563ec5f7b31c416cada251db78b6d0
techtolentino <makertolentino@gmail.com>
2020-10-17 07:50:08 -0600

    update header and copy

--- src/pages/work.js
+++ src/pages/work.js
@@ -32,7 +32,7 @@ const WorkPage = () => (
           <p>That's where I live.</p>
           <p>As a Senior UX Engineer, I work at the cross path of user experience and implementation to ensure that we deliver the best possible experiences for our users.</p>
           <p className="rt-hero-work-subtitle">This collective body of work has served people on many levels, from internal product teams to major, national nonprofit organizations, and large enterprise clients as well.</p>
-          <p><em><small>For a detailed resume and updated portfolio, please <a className="text-link-dark" href="mailto:makertolentino@gmail.com" target="_blank" rel="noopener noreferrer">send me a message</a>.</small></em></p>
+          <p><em><small>For a detailed resume and updated portfolio, please <a className="text-link-dark" href="mailto:makertolentino@gmail.com" target="_blank" rel="noopener noreferrer">send a message</a>.</small></em></p>
 
         </div>
       </div>
