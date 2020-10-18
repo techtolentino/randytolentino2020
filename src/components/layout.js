6acbaa478cfc6ae73977a9a87e770f505145f05b
techtolentino <makertolentino@gmail.com>
2020-10-18 03:41:01 -0600

    remove scroll

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,47 +1,21 @@
-import React, { Component } from "react"
+import React from "react"
+import PropTypes from 'prop-types'
 import "../styles/index.scss"
 import Header from '../components/header'
 import Footer from '../components/footer'
 
-class Layout extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visibleClass: '',
-    }
-  }
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
 
-  componentDidMount() {
-    window.onscroll = () => this.handleScroll()
-  }
-
-  handleScroll() {
-    if (document.documentElement.scrollTop > 600) {
-      this.setState({ visibleClass: 'is-visible' });
-    } else {
-      this.setState({ visibleClass: '' })
-    }
-  }
-
-  scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    })
-  }
-
-
-  render() {
-    return (
-      <>
-        <Header />
-        <main>{this.props.children}</main>
-        <Footer />
-        <button className={`rt-page-scroll-up ${this.state.visibleClass}`} onClick={this.scrollToTop}>▲</button>
-      </>
-    )
-  }
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
 }
 
 export default Layout
